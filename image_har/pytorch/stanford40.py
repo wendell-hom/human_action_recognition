2b3733c15a1b96bc00fc188200854ce39f85d892
Wendell Hom <wendell_hom@yahoo.com>
2020-03-29 15:24:12 -0700

    update pytorch code for image har

--- image_har/pytorch/stanford40.py
+++ image_har/pytorch/stanford40.py
@@ -15,24 +15,25 @@ from tqdm import tqdm
 from PIL import Image
 import pandas
 
-
+print(f"Torch version: {torch.__version__}")
+print(f"Torchvision version: {torchvision.__version__}")
 
 #%%
 
 project_name = "stanford40"
 
-pixels = 224
+pixels = 500
 rescale = int(pixels * 1.25)
 
-batch_size = 16
-test_batch_size = 64
+batch_size = 8
+test_batch_size = 32
 num_workers = 4
 num_epochs = 15
 
 learning_rate = 0.0001
 momentum = 0.9
 
-device = torch.device("cuda:1")
+device = torch.device("cuda:0")
 train_path = "data/Stanford40/train/"
 validation_path = "data/Stanford40/test/"
 
@@ -44,11 +45,18 @@ augmentation = transforms.Compose([
         transforms.ToTensor()
 ])
 
+test_transform = transforms.Compose([
+        transforms.Resize((pixels, pixels)),
+        transforms.ToTensor()
+])
+
 train_data = torchvision.datasets.ImageFolder(train_path, transform=augmentation)
 train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, num_workers=num_workers, shuffle=True )
 
-test_data = torchvision.datasets.ImageFolder(validation_path, transform=augmentation)
+test_data = torchvision.datasets.ImageFolder(validation_path, transform=test_transform)
 test_loader = torch.utils.data.DataLoader(test_data, batch_size=test_batch_size, num_workers=num_workers, shuffle=True)
+
+class_to_idx, idx_to_class = get_class_mappings(train_data)
 
 
 #%%
@@ -65,7 +73,21 @@ def train_test(start, end, history):
 
 #%%
 
-model = torchvision.models.vgg16(pretrained=True)
+model_name = "ResNet-50"
+
+if model_name == "VGG-16":
+    model = torchvision.models.vgg16(pretrained=True)
+elif model_name == "VGG-19":
+    model = torchvision.models.vgg19(pretrained=True)
+elif model_name == "ResNet-50":
+    model = torchvision.models.resnet50(pretrained=True)
+elif model_name == "MobileNet-V2":
+    model = torchvision.models.mobilenet_v2(pretrained=True)
+else:
+    model = None
+ 
+
+device = torch.device("cuda:1")
 model.to(device)
 
 #%%
@@ -77,7 +99,7 @@ optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 # Wandb setup
 # comment out if we don't want to use wandb
-setup_wandb(project_name, model, batch_size, test_batch_size, num_epochs, learning_rate, momentum, log_interval=1)
+#setup_wandb(project_name, model, batch_size, test_batch_size, num_epochs, learning_rate, momentum, log_interval=1)
 
 history = ([], [], [], [])
 train_acc, train_loss, val_acc, val_loss = train_test(1, num_epochs, history)
@@ -86,15 +108,29 @@ print("Finished training")
 
 plot(train_acc, train_loss, val_acc, val_loss)
 
+#%% 
+##### Additional Training ######
+
+set_optimizer_lr(optimizer, learning_rate/10)
+train_acc, train_loss, val_acc, val_loss = train_test(16, 30, history)
+
+plot(train_acc, train_loss, val_acc, val_loss)
 
 # %%
 
-# Save model information
-def save_model(filepath, epoch, model, optimizer, history):
-    state = {
-        'epoch': epoch,
-        'state_dict': model.state_dict(),
-        'optimizer': optimizer.state_dict(),
-        'history' : history
-    }
-    torch.save(state, filepath)
+##### Saving the model ######
+save_model("models/Stanford40/vgg19/epoch_30.pt", num_epochs, model, optimizer, history)
+
+
+
+#%%
+##### Evaluate on our own images #####
+
+test_path = "data/Stanford40/images"
+test_data = torchvision.datasets.ImageFolder(test_path, transform=test_transform)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=test_batch_size, num_workers=num_workers, shuffle=True)
+
+acc, loss = eval(model, test_loader, idx_to_class, loss_fn, device)
+
+#%%
+
--- image_har/pytorch/utilities.py
+++ image_har/pytorch/utilities.py
@@ -4,17 +4,19 @@ import torch
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 import wandb
+from apex import amp
 
-def train(model, train_loader, epoch, epochs, optimizer, loss_fn, device, train_acc=[], train_loss=[]):
+def train(model, train_loader, epoch, epochs, optimizer, loss_fn, device, train_acc=[], train_loss=[], half_precision=False):
     model.train()
 
     running_loss = 0
     correct = 0
     total = 0
-#    getattr(tqdm, '_instances', {}).clear()  # ⬅ add this line
-#    loop = tqdm(train_loader)
+    getattr(tqdm, '_instances', {}).clear()  # ⬅ add this line
+    loop = tqdm(train_loader)
 
-    for images, labels in train_loader:
+    #for images, labels in train_loader:
+    for images, labels in loop:
         images = images.to(device)
         labels = labels.to(device)
 
@@ -24,18 +26,25 @@ def train(model, train_loader, epoch, epochs, optimizer, loss_fn, device, train_
         loss = loss_fn(outputs, labels, size_average=False)
         _, predicted = torch.max(outputs, dim=1)
         correct += (predicted == labels).sum().item()
-        loss.backward()
+
+        if half_precision:
+            with amp.scale_loss(loss, optimizer) as scaled_loss:
+                scaled_loss.backward()
+        else:
+            loss.backward()
 
         optimizer.step()
 
         running_loss += loss.item()
         total += len(labels)
 
-        #loop.set_description('Epoch {}/{}'.format(epoch, epochs))
-        #loop.set_postfix(loss=loss.item())
+        loop.set_description('Epoch {}/{}'.format(epoch, epochs))
+        loop.set_postfix(loss=loss.item())
+        #print(".")
 
     loss = running_loss / total
     acc = correct / total * 100.
+    print(f"Epoch {epoch}/{epochs} acc: {acc:.4f} loss: {loss:.4f} | ", end = '')
 
     train_loss.append(loss)
     train_acc.append(acc)
@@ -72,7 +81,8 @@ def test(model, test_loader, epoch, epochs, loss_fn, device, val_acc=[], val_los
     acc = correct / total * 100.
     val_loss.append(loss)
     val_acc.append(acc)
-    print(f"Epoch {epoch}/{epochs} val_acc: {acc:.4f} val_loss: {loss:.4f}")
+    #print(f"Epoch {epoch}/{epochs} val_acc: {acc:.4f} val_loss: {loss:.4f}")
+    print(f" val_acc: {acc:.4f} val_loss: {loss:.4f}")
 
     try:
         wandb.log({"val_acc": acc, "val_loss": loss })
@@ -82,14 +92,59 @@ def test(model, test_loader, epoch, epochs, loss_fn, device, val_acc=[], val_los
     return val_acc, val_loss
 
 
+def eval(model, test_loader, idx_to_class, loss_fn, device):
+    model.eval()
+
+    correct = 0
+    total = 0
+    running_loss = 0
+    
+    example_images = []
+    with torch.no_grad():
+        for images, labels in test_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+
+            outputs = model(images)
+            loss = loss_fn(outputs, labels, size_average=False)
+            running_loss += loss.item()
+            
+            _, predicted = torch.max(outputs, dim=1)
+            correct += (predicted == labels).sum().item()
+            total += len(labels)
+
+            # Uploaded these images into W&B to view
+            for image, prediction, label in zip(images, predicted, labels):
+                prediction = idx_to_class[prediction.item()]
+                label = idx_to_class[label.item()]
+                example_images.append(wandb.Image(image, 
+                    caption="Pred: {}\nLabel: {}".format(prediction, label)))
+            
+  
+    loss = running_loss / total
+    acc = correct / total * 100.
+    print(f"Accuracy: {acc:.4f} Loss: {loss:.4f}")
+
+    try:
+        wandb.log({"example_images": example_images, "eval_acc": acc, "eval_loss": loss })
+    except:
+        pass
+
+    return acc, loss
+
+
+
 def plot(train_acc, train_loss, val_acc, val_loss):
+    plt.rcParams["figure.figsize"] = (10,4)
+    plt.subplot(1, 2, 1)
     plt.plot(train_acc)
     plt.plot(val_acc)
     plt.title('model accuracy')
     plt.ylabel('accuracy')
     plt.xlabel('epoch')
     plt.legend(['train', 'dev'], loc='upper left')
-    plt.show()
+    
+    plt.subplot(1, 2, 2)
     # summarize history for loss
     plt.plot(train_loss)
     plt.plot(val_loss)
@@ -97,6 +152,7 @@ def plot(train_acc, train_loss, val_acc, val_loss):
     plt.ylabel('loss')
     plt.xlabel('epoch')
     plt.legend(['train', 'dev'], loc='upper left')
+    plt.tight_layout()
     plt.show()
 
 
@@ -129,5 +185,15 @@ def save_model(filepath, epoch, model, optimizer, history):
 # Change the learning rate ofr the optimizer
 def set_optimizer_lr(optimizer, lr):
     for g in optimizer.param_groups:
-        g['lr'] = learning_rate/10
+        g['lr'] = lr
 
+
+
+def get_class_mappings(train_data):
+    class_to_idx = train_data.class_to_idx
+    idx_to_class = dict()
+
+    for category, idx in class_to_idx.items():
+        idx_to_class[idx] = category
+
+    return class_to_idx, idx_to_class
