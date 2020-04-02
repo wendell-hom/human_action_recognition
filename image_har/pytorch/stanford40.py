ab0a7d643fdb81b077b6e3a1fb8012159753260f
Wendell Hom <wendell_hom@yahoo.com>
2020-04-01 22:36:51 -0700

    update pytorch code

--- image_har/pytorch/stanford40.py
+++ image_har/pytorch/stanford40.py
@@ -5,7 +5,7 @@ import torch
 import torchvision
 import torch.nn as nn
 import torch.nn.functional as F
-import torchsummary
+from torchsummary import summary
 import torchvision.transforms as transforms
 from torch.utils import data
 
@@ -14,19 +14,23 @@ from utilities import *
 from tqdm import tqdm
 from PIL import Image
 import pandas
+from pathlib import Path
+import glob
 
 print(f"Torch version: {torch.__version__}")
 print(f"Torchvision version: {torchvision.__version__}")
 
+%load_ext autoreload
+%autoreload 2
 #%%
 
 project_name = "stanford40"
 
-pixels = 500
+pixels = 224
 rescale = int(pixels * 1.25)
 
-batch_size = 8
-test_batch_size = 32
+batch_size = 64
+test_batch_size = 64
 num_workers = 4
 num_epochs = 15
 
@@ -53,73 +57,120 @@ test_transform = transforms.Compose([
 train_data = torchvision.datasets.ImageFolder(train_path, transform=augmentation)
 train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, num_workers=num_workers, shuffle=True )
 
-test_data = torchvision.datasets.ImageFolder(validation_path, transform=test_transform)
-test_loader = torch.utils.data.DataLoader(test_data, batch_size=test_batch_size, num_workers=num_workers, shuffle=True)
+validation_data = torchvision.datasets.ImageFolder(validation_path, transform=test_transform)
+validation_loader = torch.utils.data.DataLoader(validation_data, batch_size=test_batch_size, num_workers=num_workers, shuffle=True)
 
 class_to_idx, idx_to_class = get_class_mappings(train_data)
 
 
 #%%
 
-def train_test(start, end, history):
-    train_acc, train_loss, val_acc, val_loss = history
-    for n in range(start, end+1):
+def train_test(num_epochs, history, directory, early_stop, patience = math.inf):
 
-        train_acc, train_loss = train(model, train_loader, n, end, optimizer, loss_fn, device, train_acc, train_loss)
-        val_acc, val_loss = test(model, test_loader, n, end, loss_fn, device, val_acc, val_loss)
+    start = len(history)+1
+    end = start + num_epochs
+
+    for n in range(start, end):
+
+        train_acc, train_loss = train(model, train_loader, n, end, optimizer, loss_fn, device)
+        val_acc, val_loss = test(model, validation_loader, n, end, loss_fn, device)
+        history.append_train_results(train_acc, train_loss)
+        improvement = history.append_val_results(val_acc, val_loss)
+
+        if improvement:
+            save_model(directory, model, optimizer, history)
     
-    return train_acc, train_loss, val_acc, val_loss
+        history = early_stop(improvement, ckpt_directory, model, optimizer, history)
+
+    return history
 
 
 #%%
 
-model_name = "ResNet-50"
+model_name = "MobileNet-V2"
+
+device = torch.device("cuda:0")
+restore_checkpoint = False
+directory = f"models/Stanford40/{model_name}/"
+
+if pixels == 224:
+    ckpt_directory = os.path.join(directory, "checkpoints_224")
+else:
+    ckpt_directory = os.path.join(directory, "checkpoints_500")
+
+Path(ckpt_directory).mkdir(parents=True, exist_ok=True)
+
 
 if model_name == "VGG-16":
     model = torchvision.models.vgg16(pretrained=True)
+    model.classifier[-1] = nn.Linear(4096, 40, True)
 elif model_name == "VGG-19":
     model = torchvision.models.vgg19(pretrained=True)
+    model.classifier[-1] = nn.Linear(4096, 40, True)
 elif model_name == "ResNet-50":
     model = torchvision.models.resnet50(pretrained=True)
+    model.fc = nn.Linear(2048, 40, True)
 elif model_name == "MobileNet-V2":
     model = torchvision.models.mobilenet_v2(pretrained=True)
+    model.classifier[-1] = nn.Linear(1280, 40, True)
+elif model_name == "ResNeXt-50":
+    model = torchvision.models.resnext50_32x4d(pretrained=True)
+    model.fc = nn.Linear(2048, 40, True)
+elif model_name == "ResNeXt-101":
+    model = torchvision.models.resnext101_32x8d(pretrained=True)
+    model.fc = nn.Linear(2048, 40, True)
+elif model_name == "DenseNet-121":
+    model = torchvision.models.densenet121(pretrained=True)
+    model.classifier = nn.Linear(1024, 40, True)
 else:
     model = None
  
 
-device = torch.device("cuda:1")
 model.to(device)
 
-#%%
-
-
 loss_fn = F.cross_entropy
+
+history = History()
+
+# try out early stop with rollback to rollback to epoch with the best weights and continue training
+# set patience to infinity if we want to disable this and use regular training
+early_stop = EarlyStopTraining(patience=3, retry_patience=6)
+
 #optimizer = torch.optim.SGD(model.parameters(), lr = learning_rate, momentum=momentum, weight_decay =0.001)
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+if restore_checkpoint:
+    epoch, history = load_model(filepath, model, optimizer)
+
+
+#%%
 
 # Wandb setup
 # comment out if we don't want to use wandb
 #setup_wandb(project_name, model, batch_size, test_batch_size, num_epochs, learning_rate, momentum, log_interval=1)
 
-history = ([], [], [], [])
-train_acc, train_loss, val_acc, val_loss = train_test(1, num_epochs, history)
-
-print("Finished training")
-
-plot(train_acc, train_loss, val_acc, val_loss)
+train_test(1000, history, ckpt_directory, early_stop)
+history.report_results()
 
 #%% 
 ##### Additional Training ######
 
 set_optimizer_lr(optimizer, learning_rate/10)
-train_acc, train_loss, val_acc, val_loss = train_test(16, 30, history)
+train_test(num_epochs, history, ckpt_directory)
+history.report_results()
 
-plot(train_acc, train_loss, val_acc, val_loss)
+#%% 
+##### Additional Training ######
+
+set_optimizer_lr(optimizer, learning_rate/100)
+train_test(num_epochs, history, ckpt_directory)
+history.report_results()
+
 
 # %%
 
 ##### Saving the model ######
-save_model("models/Stanford40/vgg19/epoch_30.pt", num_epochs, model, optimizer, history)
+save_model(f"{directory}/{pixels}_epoch_{len(history)}.pt", model, optimizer, history)
 
 
 
@@ -130,7 +181,24 @@ test_path = "data/Stanford40/images"
 test_data = torchvision.datasets.ImageFolder(test_path, transform=test_transform)
 test_loader = torch.utils.data.DataLoader(test_data, batch_size=test_batch_size, num_workers=num_workers, shuffle=True)
 
-acc, loss = eval(model, test_loader, idx_to_class, loss_fn, device)
+#%%
+# Confusion matrix
+acc, loss, ground_truth, all_predictions = eval(model, validation_loader, loss_fn, device)
+
+# Create and display the confusion matrix
+matrix = metrics.confusion_matrix(ground_truth, all_predictions)
+
+df_cm = pd.DataFrame(matrix, index = [idx_to_class[i] for i in range(40)], columns = [idx_to_class[i] for i in range(40)])
+plt.figure(figsize = (40, 40))
+sn.heatmap(df_cm, annot=True)
+
 
 #%%
 
+history = load_last_checkpoint(ckpt_directory, model, optimizer, history)
+history = load_best_checkpoint(ckpt_directory, model, optimizer, history)
+history.report_results()
+
+
+
+# %%
--- image_har/pytorch/utilities.py
+++ image_har/pytorch/utilities.py
@@ -5,8 +5,108 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 import wandb
 from apex import amp
+import math
 
-def train(model, train_loader, epoch, epochs, optimizer, loss_fn, device, train_acc=[], train_loss=[], half_precision=False):
+import pandas as pd
+import seaborn as sn
+import os
+from sklearn import metrics
+import glob
+import pdb
+
+class History():
+    def __init__(self):
+        # Where to save the models
+        self.train_acc, self.train_loss = [], []
+        self.val_acc, self.val_loss = [], []
+
+
+        #                epoch,  acc/loss
+        self.best_acc = (None, -math.inf)
+        self.best_loss = (None, math.inf)
+
+    def __len__(self):
+        return len(self.train_acc)
+
+    def append_train_results(self, train_acc, train_loss):
+        self.train_acc.append(train_acc)
+        self.train_loss.append(train_loss)
+
+    def append_val_results(self, val_acc, val_loss):
+        self.val_acc.append(val_acc)
+        self.val_loss.append(val_loss)
+
+        epoch = len(self.val_acc)
+        best_model = False
+
+        if val_acc > self.best_acc[1]:
+            self.best_acc = (epoch, val_acc)
+            #best_model = True
+        if val_loss < self.best_loss[1]:
+            self.best_loss = (epoch, val_loss)
+            best_model = True
+
+        return best_model
+
+
+    def get_best_loss(self):
+        return self.best_loss
+
+    def report_results(self):
+        if len(self.train_acc) == 0:
+            print("No results to report")
+            return
+
+        plot(self)
+        epoch_acc = self.best_acc[0]
+        epoch_loss = self.best_loss[0]
+
+        print(f'Best Accuracy at Epoch {epoch_acc} val_acc: {self.best_acc[1]:.4f}, val_loss: {self.val_loss[epoch_acc-1]:.4f}')
+        print(f'Best Loss     at Epoch {epoch_loss} val_acc: {self.val_acc[epoch_loss-1]:.4f}, val_loss: {self.best_loss[1]:.4f}')
+        print(f'Total epochs: {len(self.train_acc)}')
+
+
+class EarlyStopTraining():
+    def __init__(self, patience, retry_patience):
+        """ patience is the number of epochs to keep training without seeing loss or accuracy go down 
+            retry patience is the number of times we reload to the previous checkpoint before decreasing learning rate 
+        """
+
+        self.patience = patience
+        self.retry_patience = retry_patience
+
+        self.no_improvement = 0
+        self.num_retries = 0
+
+    def __call__(self, best_model, ckpt_directory, model, optimizer, history):
+        if best_model:
+            self.no_improvement = 0
+            self.num_retries = 0
+        else:
+            self.no_improvement += 1
+        
+        if self.no_improvement > self.patience:
+            print("num retries: ", self.num_retries)
+            history = load_best_checkpoint(ckpt_directory, model, optimizer, history)
+            self.num_retries += 1
+            print("num retries: ", self.num_retries)
+            self.no_improvement = 0
+
+            epoch, loss = history.get_best_loss()
+            print(f"Best loss so far {loss} at epoch {epoch}")
+
+            if self.num_retries > self.retry_patience:
+                # decrease learning rate by a factor of 5
+                lr = optimizer.param_groups[0]['lr'] / 5
+                set_optimizer_lr(optimizer, lr)
+                self.num_retries = 0
+                print("Reducing learning rate to ", lr)
+
+        return history 
+
+
+
+def train(model, train_loader, epoch, epochs, optimizer, loss_fn, device, half_precision=False):
     model.train()
 
     running_loss = 0
@@ -44,20 +144,19 @@ def train(model, train_loader, epoch, epochs, optimizer, loss_fn, device, train_
 
     loss = running_loss / total
     acc = correct / total * 100.
-    print(f"Epoch {epoch}/{epochs} acc: {acc:.4f} loss: {loss:.4f} | ", end = '')
 
-    train_loss.append(loss)
-    train_acc.append(acc)
+    getattr(tqdm, '_instances', {}).clear()  # ⬅ add this line
+    #print(f"\nEpoch {epoch}/{epochs} acc: {acc:.4f} loss: {loss:.4f} | ", end = '')
 
     try:
         wandb.log({"Train Accuracy": acc, "Train Loss": loss })
     except:
         pass
 
-    return train_acc, train_loss
+    return acc, loss
 
 
-def test(model, test_loader, epoch, epochs, loss_fn, device, val_acc=[], val_loss=[]):
+def test(model, test_loader, epoch, epochs, loss_fn, device):
     model.eval()
 
     correct = 0
@@ -79,20 +178,19 @@ def test(model, test_loader, epoch, epochs, loss_fn, device, val_acc=[], val_los
 
     loss = running_loss / total
     acc = correct / total * 100.
-    val_loss.append(loss)
-    val_acc.append(acc)
     #print(f"Epoch {epoch}/{epochs} val_acc: {acc:.4f} val_loss: {loss:.4f}")
-    print(f" val_acc: {acc:.4f} val_loss: {loss:.4f}")
+    getattr(tqdm, '_instances', {}).clear()  # ⬅ add this line
+    print(f"\n**** val_acc: {acc:.4f} val_loss: {loss:.4f}")
 
     try:
         wandb.log({"val_acc": acc, "val_loss": loss })
     except:
         pass
 
-    return val_acc, val_loss
+    return acc, loss
 
 
-def eval(model, test_loader, idx_to_class, loss_fn, device):
+def eval(model, test_loader, loss_fn, device):
     model.eval()
 
     correct = 0
@@ -100,6 +198,10 @@ def eval(model, test_loader, idx_to_class, loss_fn, device):
     running_loss = 0
     
     example_images = []
+
+    all_predictions = []
+    ground_truth = []
+
     with torch.no_grad():
         for images, labels in test_loader:
             images = images.to(device)
@@ -113,6 +215,9 @@ def eval(model, test_loader, idx_to_class, loss_fn, device):
             correct += (predicted == labels).sum().item()
             total += len(labels)
 
+            all_predictions.extend([y_hat.item() for y_hat in predicted])
+            ground_truth.extend([label.item() for label in labels])
+
             # Uploaded these images into W&B to view
             for image, prediction, label in zip(images, predicted, labels):
                 prediction = idx_to_class[prediction.item()]
@@ -120,7 +225,7 @@ def eval(model, test_loader, idx_to_class, loss_fn, device):
                 example_images.append(wandb.Image(image, 
                     caption="Pred: {}\nLabel: {}".format(prediction, label)))
             
-  
+    # Calculate and print out information on accuracy and loss
     loss = running_loss / total
     acc = correct / total * 100.
     print(f"Accuracy: {acc:.4f} Loss: {loss:.4f}")
@@ -130,11 +235,15 @@ def eval(model, test_loader, idx_to_class, loss_fn, device):
     except:
         pass
 
-    return acc, loss
+    return acc, loss, ground_truth, all_predictions
 
 
 
-def plot(train_acc, train_loss, val_acc, val_loss):
+
+def plot(history):
+    train_acc, train_loss = history.train_acc, history.train_loss
+    val_acc, val_loss = history.val_acc, history.val_loss
+
     plt.rcParams["figure.figsize"] = (10,4)
     plt.subplot(1, 2, 1)
     plt.plot(train_acc)
@@ -173,14 +282,30 @@ def setup_wandb(project_name, model, batch_size, test_batch_size, num_epochs, le
 
 
 # Save model information
-def save_model(filepath, epoch, model, optimizer, history):
+def save_model(directory, model, optimizer, history):
+    epoch = len(history)
     state = {
         'epoch': epoch,
         'state_dict': model.state_dict(),
         'optimizer': optimizer.state_dict(),
         'history' : history
     }
-    torch.save(state, filepath)
+    filename = os.path.join(directory, f'epoch_{epoch}.pt')
+    #print("Saving model to ", filename)
+    torch.save(state, filename)
+
+def load_model(filepath, model, optimizer):
+
+    print("Loading " + filepath)
+
+    checkpoint = torch.load(filepath)
+    model.load_state_dict(checkpoint['state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer'])
+    epoch = checkpoint['epoch']
+    history = checkpoint['history']
+
+    return epoch, history
+
 
 # Change the learning rate ofr the optimizer
 def set_optimizer_lr(optimizer, lr):
@@ -197,3 +322,25 @@ def get_class_mappings(train_data):
         idx_to_class[idx] = category
 
     return class_to_idx, idx_to_class
+
+
+def load_best_checkpoint(ckpt_directory, model, optimizer, history):
+    epoch = history.best_loss[0]
+
+    filepath = os.path.join(ckpt_directory, f"epoch_{epoch}.pt")
+
+    epoch, history = load_model(filepath, model, optimizer)
+
+    return history
+
+def load_last_checkpoint(ckpt_directory, model, optimizer, history):
+
+    checkpoints = os.path.join(ckpt_directory, f"epoch_*.pt")
+    checkpoints = glob.glob(checkpoints)
+    checkpoints.sort(key=os.path.getmtime, reverse=True)
+    filepath = checkpoints[0]
+
+    epoch, history = load_model(filepath, model, optimizer)
+    return history
+
+   
